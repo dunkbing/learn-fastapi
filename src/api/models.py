29f2889aeb65706d3a8758971f1573bd5c103a32
binh.buidang <dangbinh4869@gmail.com>
2022-02-22 17:03:55 +0700

    feat: camel case api response.

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,39 @@
 from datetime import datetime
+from pydantic import BaseModel
 from sqlalchemy import Column, DateTime, event
+
+
+def camelize(string: str) -> str:
+
+    assert isinstance(string, str), 'Input must be of type str'
+
+    first_alphabetic_character_index = -1
+    for index, character in enumerate(string):
+        if character.isalpha():
+            first_alphabetic_character_index = index
+            break
+
+    empty = ''
+
+    if first_alphabetic_character_index == -1:
+        return empty
+
+    string = string[first_alphabetic_character_index:]
+
+    titled_string_generator = (
+        character for character in string.title() if character.isalnum())
+
+    try:
+        return next(titled_string_generator).lower() + empty.join(titled_string_generator)
+
+    except StopIteration:
+        return empty
+
+
+class CamelModel(BaseModel):
+    class Config:
+        alias_generator = camelize
+        allow_population_by_field_name = True
 
 
 class TimeStampMixin(object):
